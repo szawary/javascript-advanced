5ee1042b448e38f1e8b2ffbc3ad5a1b4ee4ef199
Szabo Laszlo <szabo.laszlo@szawary.eu>
2020-12-30 09:58:46 +0100

    algo task 1-6

---
+++ algorithmization-exercise/000-bubble-sorting.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function bubbleSort(arr) {
+    let swapped;                           // ebben a változóban csak azt tárolom, hogy a tömb rendezett-e vagy sem.
+
+    do {                                  // egyszer mindenképpen le fog futni
+        swapped = false;                  // azt mondjuk hogy biztos nem rendezett még
+        for (let i = 0; i < arr.length - 1; i += 1) {
+            if (arr[i] > arr[i + 1]) {    // ha kisebb mint a soron következő
+                let temp = arr[i];
+                arr[i] = arr[i + 1];      // az elemeket megcseréljük. 
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped)
+    return arr;
+}
